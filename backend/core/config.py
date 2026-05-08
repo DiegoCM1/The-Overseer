58a4e5e4fe0c62351cc138192b3def34abc69d3d
DiegoCM1 <luiscolin764@gmail.com>
2026-05-08 14:40:36 -0600

    feat: added db connection and lifespan lifecycle in fastapi for handling startup events correctly. Created table on Railway postgress

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,14 +4,21 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
+    # AI
     OPENROUTER_API_KEY: str
+    
+    # MODE
     APP_ENV: str
 
-    # WA variables
+    # DB
+    DATABASE_URL: str
+    
+    
+    # WA
     TWILIO_ACCOUNT_SID: str
-    TWILIO_AUTH_TOKEN : str
-    TWILIO_WHATSAPP_FROM:str                                                
-    TWILIO_WHATSAPP_TO:str
+    TWILIO_AUTH_TOKEN: str
+    TWILIO_WHATSAPP_FROM: str                                            
+    TWILIO_WHATSAPP_TO: str
 
     class Config:
         env_file = BASE_DIR / ".env"
