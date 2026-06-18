e3b9d53ad6b383c48a91690cde365068b8371f7d
DiegoCM1 <luiscolin764@gmail.com>
2026-06-17 20:07:54 -0600

    feat: mvp of functional agent from scratch, no frameworks

--- backend/features/agent/agent.py
+++ backend/features/agent/agent.py
@@ -12,20 +12,68 @@ client = OpenAI(
 
 
 # Then we create a list of inputs that contains dictionaries, one per input
-input_list = [
+messages = [
     { 
         "role": "user",
-        "content": "This is my first prompt, tell me the current time in Portugal"
+        "content": "Tell me the current time in Tokyo, also, tell me what's the result of 8*8+5+31-99"
     }
 ]
 
-print(f"This is the input: {input_list}")
+print(f"This is the input: {messages}")
 
-# Invoke llm and save response
-response = client.responses.create(
-    model="deepseek/deepseek-v4-flash", # Using a model from openrouter
-    tools=tools, # Load tools to model
-    input=input_list # Sending the input to the model
-)
 
-print(f"This is the response: {response}")
+finished = False
+isToolPending = False
+
+while not finished:
+    isToolPending = False
+
+    # Invoke llm and save response
+    response = client.responses.create(
+        model="deepseek/deepseek-v4-flash", # Using a model from openrouter
+        tools=tools, # Load tools to model
+        input=messages # Sending the input to the model
+    )
+
+    # Save tool call outputs for next requests
+    messages += response.output
+
+    for item in response.output:
+        if item.type == "function_call":
+            # Flag for tools being pending
+            isToolPending = True
+
+            # Iterate over each tool
+            if item.name == "get_current_datetime":
+                timezone = json.loads(item.arguments)["timezone"]
+                time_result = get_current_datetime(timezone)        
+                # Append tool results
+                messages.append({
+                    "type": "function_call_output",
+                    "call_id": item.call_id,
+                    "output": time_result
+                })
+                print("Executed get_current_datetime")
+
+
+            if item.name == "do_math":
+                expression = json.loads(item.arguments)["expression"]
+                result = do_math(expression)
+                # Append tool results
+                messages.append({
+                    "type": "function_call_output",
+                    "call_id": item.call_id,
+                    "output": result
+                })
+                print("Executed do_math")
+
+    if not isToolPending:
+        finished = True
+
+for item in response.output:
+    if item.type == "reasoning":
+        print(f"""--Reasoning {item.content[0].text} --Reasoning--""")
+    if item.type == "message":
+        print(item.content[0].text)
+
+
